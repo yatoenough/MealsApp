c083adb92dd6d075a99c3cd1bdd81f31d72b8559
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-19 12:14:01 +0200

    Added mealitem and displaying items on meal view

--- lib/view/categories_view.dart
+++ lib/view/categories_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/model/category.dart';
+import 'package:meals/model/meal.dart';
 import 'package:meals/view/meals_view.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesView extends StatelessWidget {
   const CategoriesView({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    List<Meal> filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => const MealsView(title: 'Title', meals: []),
+        builder: (context) =>
+            MealsView(title: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -33,7 +42,7 @@ class CategoriesView extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onCategorySelected: () => _selectCategory(context),
+              onCategorySelected: () => _selectCategory(context, category),
             )
         ],
       ),
--- lib/view/meals_view.dart
+++ lib/view/meals_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/model/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsView extends StatelessWidget {
   const MealsView({super.key, required this.title, required this.meals});
@@ -14,12 +15,33 @@ class MealsView extends StatelessWidget {
         title: Text(title),
       ),
       body: meals.isEmpty
-          ? const Center(
-              child: Text("No meals avaliable."),
+          ? Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Uh oh... No meals avaliable!",
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Try selecting a different category!",
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  )
+                ],
+              ),
             )
           : ListView.builder(
-            itemCount: meals.length,
-              itemBuilder: (context, index) => Text(meals[index].title),
+              itemCount: meals.length,
+              itemBuilder: (context, index) => MealItem(
+                meal: meals[index],
+              ),
             ),
     );
   }
