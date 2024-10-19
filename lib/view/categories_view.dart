be9f4c463fcb457ce457170488a40ab67300f0fb
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-19 11:06:13 +0200

    Added categories grid view

---
+++ lib/view/categories_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your category"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text("1"),
+          Text("2"),
+          Text("3"),
+          Text("4"),
+          Text("5"),
+          Text("6")
+        ],
+      ),
+    );
+  }
+}
