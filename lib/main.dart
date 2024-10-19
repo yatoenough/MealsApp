6ea038a26e593fe28f672227930756e1f1bf9962
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-19 10:56:22 +0200

    added google fonts, inited theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals/themes/main_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Meals App',
+      theme: mainTheme,
       home: const Scaffold(),
     );
   }
