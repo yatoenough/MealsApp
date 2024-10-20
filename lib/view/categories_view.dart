a1cb9abe251e961d6ce4aecb8576be1832ac3b46
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-20 10:55:07 +0200

    Improved categories animation

--- lib/view/categories_view.dart
+++ lib/view/categories_view.dart
@@ -27,7 +27,7 @@ class _CategoriesViewState extends State<CategoriesView>
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 800),
       lowerBound: 0,
       upperBound: 1,
     );
@@ -80,9 +80,13 @@ class _CategoriesViewState extends State<CategoriesView>
         ],
       ),
       builder: (context, child) {
-        return Padding(
-          padding: EdgeInsets.only(
-            top: 100 - _animationController.value * 100,
+        return SlideTransition(
+          position: Tween(
+            begin: const Offset(0, 1),
+            end: const Offset(0, 0),
+          ).animate(
+            CurvedAnimation(
+                parent: _animationController, curve: Curves.easeInOut),
           ),
           child: child,
         );
